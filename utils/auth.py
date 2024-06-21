09eec10ee9c67b3643692076854769a0351cf888
bdobosevych <bohdan.dobosevych@gmail.com>
2024-06-21 14:29:24 +0300

    SPRY-76 add verify_firebase_token

--- utils/auth.py
+++ utils/auth.py
@@ -2,6 +2,8 @@ from fastapi import Header, HTTPException, Depends
 
 from sqlalchemy.orm import Session
 
+from .firebase import verify_firebase_token
+
 from models import get_db
 from models.repositories.user_repository import UserRepository
 
@@ -9,7 +11,8 @@ from models.repositories.user_repository import UserRepository
 def get_user(token: str = Header(None, convert_underscores=False), db: Session = Depends(get_db)):
     user_repository = UserRepository(db)
     try:
-        user = user_repository.find_by_email(token)
+        email = verify_firebase_token(token)
+        user = user_repository.find_by_email(email)
         if user is None:
             raise HTTPException(status_code=401, detail="Invalid auth token")
         return user
