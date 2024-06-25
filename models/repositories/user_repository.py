fcdcc34a48ae5146021f4d6aae3f9717da27eddc
bdobosevych <bohdan.dobosevych@gmail.com>
2024-06-25 11:26:18 +0300

    SPRY-76 update User

--- models/repositories/user_repository.py
+++ models/repositories/user_repository.py
@@ -11,14 +11,9 @@ class UserRepository(BaseRepo[User]):
     def __init__(self, session):
         super().__init__(session, User)
 
-    def find_by_id(self, user_id: int, status: str = User.STATUS_ACTIVE) -> User:
-        return self.session.query(User).filter(User.id == user_id).filter(User.status == status).first()
-
     def find_by_email(self, email: str) -> User:
         return self.session.query(User).filter(User.email == email).first()
 
-    def find_by_invite_token(self, invite_token: str, status: str = User.STATUS_NEW) -> User:
-        return self.session.query(User).filter(User.invite_token == invite_token).filter(User.status == status).first()
 
 
 class HierarchyRepository(BaseRepo[Hierarchy]):
