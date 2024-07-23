c00fcd8ca5ffb5f1b60a19ece5fb30580a9705cc
bdobosevych <bohdan.dobosevych@gmail.com>
2024-07-23 15:12:27 +0300

    SPRY-89 add get /team

--- service_views/team.py
+++ service_views/team.py
@@ -1,10 +1,26 @@
-from fastapi import APIRouter
+from fastapi import FastAPI, Request, HTTPException, Depends, APIRouter, Form
+
+from sqlalchemy.orm import Session
+
+from models import get_db, User, Team, TeamMember
+from models.repositories.user_repository import UserRepository, TeamRepository, TeamMemberRepository
+from utils.auth import get_user
+
 
 router = APIRouter()
 
-
 @router.get("/team/")
-def team():
+def team(user: User = Depends(get_user), db: Session = Depends(get_db)):
+    team_repository = TeamRepository(db)
+    team = team_repository.find_by_create_user_id(user.id)
+    if team is None:
+        return []
+    return team_repository.find_by_team_member(team.id)
+
+
+
+@router.get("/team/chart/")
+def team_chart():
     return {"data": [
         ['Date', 'Recurring', 'One-time'],
         ['Jun 19', 70, 30],
--- service_views/user.py
+++ service_views/user.py
@@ -6,16 +6,11 @@ from pydantic import BaseModel
 
 from sqlalchemy.orm import Session
 
-from models import get_db, User, InvitedUser
-from models.repositories.user_repository import UserRepository
-from models.repositories.user_repository import InvitedUserRepository
+from models import get_db, User, Team, TeamMember
+from models.repositories.user_repository import UserRepository, TeamRepository, TeamMemberRepository
 from utils.auth import get_user
 
 router = APIRouter()
-
-
-def generate_unique_token():
-    return uuid.uuid4().hex
 
 
 class EmailRequest(BaseModel):
@@ -24,14 +19,17 @@ class EmailRequest(BaseModel):
 
 @router.post("/user/invite/")
 async def user_invite(email_request: EmailRequest, user: User = Depends(get_user), db: Session = Depends(get_db)):
-    user_repository = UserRepository(db)
-    invited_user_repository = InvitedUserRepository(db)
+    team_repository = TeamRepository(db)
+    team_member_repository = TeamMemberRepository(db)
+    print(user.id)
+    team = team_repository.find_by_create_user_id(user.id)
+    if not team:
+        team = Team(create_user_id=user.id)
+        team_repository.create(team)
 
-    new_user = User(email=email_request.email)
-    user_repository.create(new_user)
-
-    invited_user = InvitedUser(
-        user_id=new_user.id,
+    team_member = TeamMember(
+        team_id=team.id,
+        email=email_request.email,
         added_by_id=user.id
     )
-    invited_user_repository.create(invited_user)
+    team_member_repository.create(team_member)
