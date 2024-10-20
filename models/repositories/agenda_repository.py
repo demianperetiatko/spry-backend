83ac080fe4a13b51cf6a4fc4f98cedac0ddc63da
bdobosevych <bohdan.dobosevych@gmail.com>
2024-10-21 00:20:48 +0300

    SPRY-130 update find_by_id_and_user_id

--- models/repositories/agenda_repository.py
+++ models/repositories/agenda_repository.py
@@ -15,4 +15,4 @@ class AgendaTemplateRepository(BaseRepo[AgendaTemplate]):
         return self.session.query(AgendaTemplate).filter_by(create_user_id=create_user_id).all()
 
     def find_by_id_and_user_id(self, id_: int, user_id: int) -> List[AgendaTemplate]:
-        return self.session.query(AgendaTemplate).filter_by(id=id_, create_user_id=user_id).all()
+        return self.session.query(AgendaTemplate).filter_by(id=id_, create_user_id=user_id).first()
