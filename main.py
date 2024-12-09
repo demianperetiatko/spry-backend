4a489f63b2f78fb91a4ad69b090d195cbf6270dc
bdobosevych <bohdan.dobosevych@gmail.com>
2024-12-10 01:02:03 +0200

    SPRY update

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000", "https://app.spryplan.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- service_views/auth.py
+++ service_views/auth.py
@@ -3,9 +3,9 @@ from fastapi import APIRouter, Request,  Depends
 from sqlalchemy.orm import Session
 from starlette.responses import RedirectResponse
 
-from models import get_db
+from models import get_db, User
 
-from utils.services import create_google_login_uri, update_user_after_google_login
+from utils.services import create_google_login_uri, update_user_after_google_login, authenticated_user
 
 
 router = APIRouter(
@@ -15,19 +15,29 @@ router = APIRouter(
 
 FRONTEND_URL = "https://app.spryplan.com/" if os.getenv('APP_ENV') == "prod" else "http://localhost:3000/"
 
+@router.get("/")
+async def auth(user: User = Depends(authenticated_user)):
+    return {
+        "id": user.id,
+        "email": user.email,
+        "name": user.name,
+    }
 
-@router.get("/google")
+@router.get("/google/")
 async def login_google():
     uri = create_google_login_uri()
     return {
         "uri": uri,
     }
 
-
-@router.get("/callback/google")
+@router.get("/callback/google/")
 async def auth_google(request: Request, db: Session = Depends(get_db)):
     state = request.query_params.get("state")
     authorization_response = str(request.url)
     user = update_user_after_google_login(state, authorization_response, db)
     request.session["user_id"] = user.id
     return RedirectResponse(FRONTEND_URL)
+
+@router.get("/logout/")
+async def logout(request: Request):
+    request.session.clear()
