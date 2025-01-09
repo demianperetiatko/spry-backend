b6a689cc9a49bd35593835fde498c50a3f6bda25
bdobosevych <bohdan.dobosevych@gmail.com>
2025-01-09 17:47:23 +0200

    SPRY add delete

--- service_views/auth.py
+++ service_views/auth.py
@@ -1,5 +1,5 @@
 import os
-from fastapi import APIRouter, Request,  Depends
+from fastapi import APIRouter, Request, Depends
 from sqlalchemy.orm import Session
 from starlette.responses import RedirectResponse
 
@@ -7,13 +7,13 @@ from models import get_db, User
 
 from utils.services import create_google_login_uri, update_user_after_google_login, authenticated_user
 
-
 router = APIRouter(
     prefix='/auth',
     tags=['auth']
 )
 
 FRONTEND_DOMAIN = "https://app.spryplan.com" if os.getenv('APP_ENV') == "prod" else "http://localhost:3000"
+
 
 @router.get("/")
 async def auth(user: User = Depends(authenticated_user)):
@@ -23,12 +23,14 @@ async def auth(user: User = Depends(authenticated_user)):
         "name": user.name,
     }
 
+
 @router.get("/google/")
 async def login_google():
     uri = create_google_login_uri()
     return {
         "uri": uri,
     }
+
 
 @router.get("/callback/google/")
 async def auth_google(request: Request, db: Session = Depends(get_db)):
@@ -45,6 +47,12 @@ async def auth_google(request: Request, db: Session = Depends(get_db)):
     redirect_url = f"{FRONTEND_DOMAIN}/invite-members" if is_new_user else FRONTEND_DOMAIN
     return RedirectResponse(redirect_url)
 
+
 @router.get("/logout/")
 async def logout(request: Request):
+    request.session.clear()
+
+
+@router.get("/delete")
+async def delete_user(request: Request, user: User = Depends(authenticated_user)):
     request.session.clear()
