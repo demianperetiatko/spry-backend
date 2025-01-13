a586542307e3b696d4c825a037d4be0359be1706
bdobosevych <bohdan.dobosevych@gmail.com>
2025-01-13 10:03:48 +0200

    SPRY update

--- models/repositories/organization_repository.py
+++ models/repositories/organization_repository.py
@@ -13,15 +13,31 @@ class OrganizationRepository(BaseRepo[Organization]):
     def __init__(self, session):
         super().__init__(session, Organization)
 
-    def find_organization(self, user_id: int) -> Organization:
-        return self.session.query(Organization).filter(Organization.create_user_id == user_id).first()
+    def find_organization(self, user: User) -> Organization:
+        return (
+            self.session.query(Organization)
+            .join(OrganizationMember, Organization.id == OrganizationMember.organization_id, isouter=True)
+            .filter(
+                (Organization.create_user_id == user.id) |
+                (OrganizationMember.email == user.email)
+            )
+            .first()
+        )
 
 
 class OrganizationMemberRepository(BaseRepo[OrganizationMember]):
     def __init__(self, session):
         super().__init__(session, OrganizationMember)
 
-    def find_member(self, organization_id: int) -> Organization:
+    def find_member(self, organization_id: int, member_id: int) -> OrganizationMember:
+        return (
+            self.session.query(OrganizationMember)
+            .filter(OrganizationMember.create_user_id == member_id)
+            .filter(OrganizationMember.organization_id == organization_id)
+            .first()
+        )
+
+    def find_members(self, organization_id: int) -> Organization:
         return (
             self.session.query(
                 OrganizationMember.id,
--- service_views/organization.py
+++ service_views/organization.py
@@ -16,8 +16,8 @@ router = APIRouter()
 def get_member(user: User = Depends(authenticated_user), db: Session = Depends(get_db)):
     org_repository = OrganizationRepository(db)
     organization_member_repository = OrganizationMemberRepository(db)
-    org = org_repository.find_organization(user.id)
-    members = organization_member_repository.find_member(org.id)
+    org = org_repository.find_organization(user)
+    members = organization_member_repository.find_members(org.id)
     return {
         'data': members,
         'total_count': len(members)
@@ -36,7 +36,7 @@ def add_members_to_organization(
 ):
     organization_repository = OrganizationRepository(db)
     organization_member_repository = OrganizationMemberRepository(db)
-    org = organization_repository.find_organization(user.id)
+    org = organization_repository.find_organization(user)
     if not org:
         org = Organization(create_user_id=user.id)
         organization_repository.create(org)
@@ -59,3 +59,34 @@ def add_members_to_organization(
                        <p>If you have any questions, feel free to contact us.</p>
                    """)
         organization_member_repository.create(member)
+
+
+class UpdateMemberRequest(BaseModel):
+    email: EmailStr
+
+
+@router.put("/member/{member_id}")
+def update_member(
+        member_id: int,
+        member_info: UpdateMemberRequest,
+        user: User = Depends(authenticated_user),
+        db: Session = Depends(get_db)
+):
+    return
+
+
+@router.delete("/member/{member_id}")
+def delete_member_from_organization(
+        member_id: int,
+        user: User = Depends(authenticated_user),
+        db: Session = Depends(get_db)
+):
+    organization_repository = OrganizationRepository(db)
+    organization_member_repository = OrganizationMemberRepository(db)
+    org = organization_repository.find_organization(user)
+    if not org:
+        raise HTTPException(status_code=404, detail="Organization not found")
+    member = organization_member_repository.find_member(org.id, member_id)
+    if not member or member.organization_id != org.id:
+        raise HTTPException(status_code=404, detail="Member not found")
+    organization_member_repository.delete(member)
