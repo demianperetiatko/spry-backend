a8c10511f1e426b13a7b5de2518f0f6e3bc522f0
bdobosevych <bohdan.dobosevych@gmail.com>
2025-04-07 17:51:05 +0300

    SPRY update calculate_chance

--- utils/analytics/util.py
+++ utils/analytics/util.py
@@ -1,2 +1,9 @@
 def calculate_chance(new, old):
-    return round(((new - old) / old) * 100)
+    if old == 0:
+        if new == 0:
+            return 0
+        elif new > 0:
+            return 100
+        else:
+            return -100
+    return round(((new - old) / old) * 100, 2)
