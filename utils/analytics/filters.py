7091a8a2fe8897a32e1c88ed01679651cf692d06
bdobosevych <bohdan.dobosevych@gmail.com>
2025-06-04 15:01:30 +0300

    SPRY update filter_meetings

--- utils/analytics/filters.py
+++ utils/analytics/filters.py
@@ -2,6 +2,8 @@ def filter_meetings(events: list) -> list:
     meetings = []
     for event in events:
         attendees = event.get("attendees", [])
-        if len(attendees) > 2:
+        start_str = event.get("start", {}).get("dateTime")
+        end_str = event.get("end", {}).get("dateTime")
+        if start_str and end_str and len(attendees) >= 2:
             meetings.append(event)
     return meetings
