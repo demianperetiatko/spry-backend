e94fd901886100f9975f11ff35c794c4b5f7e51f
dobosevych <dobosevych@gmail.com>
2026-05-03 12:49:48 +0300

    Fixed format

--- src/modules/user/model.py
+++ src/modules/user/model.py
@@ -30,7 +30,9 @@ class User(Base):
         default=UserStatusEnum.PENDING,
     )
 
-    access_info: Mapped["UserAccessInfo"] = relationship("UserAccessInfo", back_populates="user", cascade="all, delete-orphan")
+    access_info: Mapped["UserAccessInfo"] = relationship(
+        "UserAccessInfo", back_populates="user", cascade="all, delete-orphan",
+    )
     organization_members: Mapped[list["OrganizationMember"]] = relationship(
         "OrganizationMember",
         back_populates="user",
